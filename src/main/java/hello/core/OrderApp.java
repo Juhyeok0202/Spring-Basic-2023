b8d7723d2b0f2eacf8de6dfaad19e8912ff8cee1
LIM <ovg07047@naver.com>
2023-06-26 14:19:55 +0900

    feat: 스프링 기능 없이 main func에서 테스트

---
+++ src/main/java/hello/core/OrderApp.java
@@ -0,0 +1,28 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServuceImpl;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class OrderApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServuceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Member memberA = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(memberA);
+        Member memberB = new Member(2L, "memberB", Grade.BASIC);
+        memberService.join(memberB);
+
+        Order orderA = orderService.createOrder(memberA.getId(), "itemA", 10000);
+        Order orderB = orderService.createOrder(memberB.getId(), "itemA", 10000);
+
+        System.out.println(orderA+ "\n" + orderA.calculatePrice());
+        System.out.println(orderB+ "\n" + orderB.calculatePrice());
+    }
+}
