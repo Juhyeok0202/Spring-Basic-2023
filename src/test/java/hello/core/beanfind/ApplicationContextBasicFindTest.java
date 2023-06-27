9c72984b41d9b2418deb537a0e347c64bad826b3
LIM <ovg07047@naver.com>
2023-06-27 18:22:55 +0900

    Test: 구체 타입으로 빈 조회

--- src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -30,4 +30,14 @@ class ApplicationContextBasicFindTest {
     }
 
 
+    //구체적인 타입으로 조회할 수도 있다.
+    //좋은 방법은 아님. DIP에 의하면 구현보다는 역할에 의존해야함. interface에 의존하는게 더 좋음
+    //좋은 코드는 아니지만, 예외적으로 필요할 때가 있다.
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2() {
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
 }
